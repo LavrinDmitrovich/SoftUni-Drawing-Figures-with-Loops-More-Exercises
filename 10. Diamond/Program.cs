3d8d85366c9cfeea3cb892a2f890f0f2601fc013
LavrinDmitrovich <dmitrylavrinovich@gmail.com>
2023-04-18 15:55:21 +0300

    the whole one

---
+++ 10. Diamond/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace _10._Diamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numnum2 = int.Parse(Console.ReadLine());
+            int numnum = numnum2 - 2;
+            int num = 0;
+            int num2 = 0;
+
+            if (numnum % 2 == 0) { num = numnum / 2; }
+            else { num = (numnum + 1) / 2; }
+            if (numnum2 % 2 == 0) { num2 = numnum2 / 2; }
+            else { num2 = (numnum2 + 1) / 2; }
+
+            if (numnum2<=2) { goto label; }
+
+            // first line
+            for (int i = num2 - 1; i > num2 - 2; i--)
+            {
+                for (int j = 0; j < i; j++) { Console.Write("-"); }
+                for (int j = 0; j < numnum2 - 2 * i; j++) { Console.Write("*"); }
+                for (int j = i - 1; j >= 0; j--) { Console.Write("-"); }
+                Console.Write("\n");
+            }
+
+            // first half
+            for (int i = num -1 ; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++) { Console.Write("-"); }
+                Console.Write("*");
+                for (int j = 0; j < numnum - 2 * i; j++) { Console.Write("-"); }
+                Console.Write("*");
+                for (int j = i ; j > 0; j--) { Console.Write("-"); }
+                Console.Write("\n");
+            }
+
+            // second half
+            for (int i =  1; i < num; i++)
+            {
+                for (int j = 0; j < i; j++) { Console.Write("-"); }
+                Console.Write("*");
+                for (int j = 0; j < numnum - 2 * i; j++) { Console.Write("-"); }
+                Console.Write("*");
+                for (int j = i; j > 0; j--) { Console.Write("-"); }
+                Console.Write("\n");
+            }
+            // last line
+
+            label:
+            for (int i = num2 - 1; i > num2 - 2; i--)
+            {
+                for (int j = 0; j < i; j++) { Console.Write("-"); }
+                for (int j = 0; j < numnum2 - 2 * i; j++) { Console.Write("*"); }
+                for (int j = i - 1; j >= 0; j--) { Console.Write("-"); }
+                Console.Write("\n");
+            }
+
+            
+
+        }
+    }
+}
---
+++ 9. house/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _9._house
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numnum = int.Parse(Console.ReadLine());
+            int num = 0;
+
+            if (numnum%2 ==0) { num = numnum / 2; }
+            else { num = (numnum+1) / 2; }
+
+            // first half
+
+            for (int i = num - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                { Console.Write("-"); }
+
+                for (int j = 0; j < numnum - 2*i; j++)
+                { Console.Write('*'); }
+
+                for (int j = i-1; j >= 0; j-- )
+                { Console.Write("-"); }
+
+
+                Console.Write("\n");
+            }
+
+            for (int i = 0; i < numnum - num; i++) 
+            {
+                Console.Write("|");
+                for (int j = 0;j < numnum-2; j++) {Console.Write("*"); }
+                Console.Write("|");
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
